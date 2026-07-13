57e6bc796ee2c4b10ce8a4a0b9b91f477a4c1e8b
Cajosh24 <152527095+Cajosh24@users.noreply.github.com>
2026-07-13 12:39:36 -0700

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_dict = defaultdict(int)
+        t_dict = defaultdict(int)
+        
+        for char in s:
+            s_dict[char] += 1
+        for char in t:
+            t_dict[char] += 1
+
+        return t_dict == s_dict
