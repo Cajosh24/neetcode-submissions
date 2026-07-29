9ebcca45051e656e24e0b99da4e71555f4f00d1f
Cajosh24 <152527095+Cajosh24@users.noreply.github.com>
2026-07-29 11:41:43 -0700

    Add: longest-substring-without-duplicates - submission-3

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-3.py
@@ -0,0 +1,32 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if not s: 
+            return 0
+
+        seen = set()
+        l = 0
+        r = l
+        best_length = 0
+        current_length = 0
+
+        while r < len(s):
+            if s[r] not in seen: #add char to streak
+                seen.add(s[r])
+                current_length += 1
+            elif s[l] == s[r]: #move sliding window
+                l += 1
+            else: #reset streak
+                seen.clear()
+                seen.add(s[r])
+
+                current_length = 1
+                l = r
+
+            best_length = max(best_length,current_length)
+            
+            #increment right index
+            r += 1
+
+        return best_length
+
+
