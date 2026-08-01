78e75767dfd59923a2da445601e648e70b85443e
Cajosh24 <152527095+Cajosh24@users.noreply.github.com>
2026-08-01 01:02:01 -0700

    Add: trapping-rain-water - submission-8

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-8.py
@@ -0,0 +1,47 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0  
+
+        total = 0
+        peak_height = max(height)
+
+        if peak_height == 0:
+            return 0
+        
+        peak_index = height.index(peak_height)
+
+        #find left local minimmum
+        l = 0
+        while height[l] == 0:
+            l += 1
+        local_max = height[l]
+        
+        #keep iterating through l pointer until reach peak while counting water
+        l += 1
+        while l < peak_index:
+            if local_max <= height[l]:
+                local_max = height[l]
+            else:
+                total += local_max - height[l]
+            l += 1
+
+        
+        #find right local minimum
+        r = len(height) - 1
+        while height[r] == 0:
+            r -= 1
+        local_max = height[r]
+        
+        #keep iterating through r pointer until reach peak while counting water
+        r -= 1
+        while r > peak_index:
+            if local_max <= height[r]:
+                local_max = height[r]
+            else:
+                total += local_max - height[r]
+            r -= 1
+
+        return total
+
+        
